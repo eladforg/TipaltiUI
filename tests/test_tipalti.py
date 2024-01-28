1bc5c606428e76b89596ebfede9004d13fa4c0c7
eladforg <elad.forgash@gmail.com>
2024-01-28 14:30:18 +0200

    tipalti after test

--- tests/test_tipalti.py
+++ tests/test_tipalti.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -12,20 +14,22 @@ menu_items_texts =[]
 for item in menu_items:
     text = item.text
     menu_items_texts.append(text)
+# print(menu_items_texts)
 
 isKika=False
 for text in menu_items_texts:
     if text=="Kika":
         isKika=True
 
+# print(isKika)
 # menu_items_texts['Kika'].click()
 # or;
 driver.find_element(By.CSS_SELECTOR,"#menu > div > ul > li:nth-child(2) > a").click()
-
+time.sleep(1)
 driver.find_element(By.ID, "name").send_keys("Cookie")
 driver.find_element(By.ID, "email").send_keys("cookie@gmail.com")
 driver.find_element(By.ID, "message").send_keys("this is a message")
-driver.find_element(By.CSS_SELECTOR, "['value'='send']").click()
+# driver.find_element(By.CSS_SELECTOR, "[value='send']").click()
 
 
 input("click")
