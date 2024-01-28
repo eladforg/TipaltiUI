9c4427db26bc9e071ba0d83d9e603720d65a4b1b
eladforg <elad.forgash@gmail.com>
2024-01-28 14:23:41 +0200

    tipalti after test

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,17 +12,17 @@ from requests import Response
 
 
 # for setting a global driver to be created before any run of class:
-@pytest.fixture(scope='class', autouse=True)
-def driver_init(request):
-    global driver
-    options = Options()
-    options.add_experimental_option("detach", True)
-    driver = webdriver.Chrome(options=options)
-    request.cls.driver = driver
-    driver.get("https://www.saucedemo.com/")
-    driver.maximize_window()
-    yield
-    # driver.quit()
+# @pytest.fixture(scope='class', autouse=True)
+# def driver_init(request):
+#     global driver
+#     options = Options()
+#     options.add_experimental_option("detach", True)
+#     driver = webdriver.Chrome(options=options)
+#     request.cls.driver = driver
+#     driver.get("https://www.saucedemo.com/")
+#     driver.maximize_window()
+#     yield
+#     # driver.quit()
 
 
 # to create allure results report after every run:
---
+++ tests/test_tipalti.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get("http://qa-tipalti-assignment.tipalti.com/index.html")
+
+driver.find_element(By.CSS_SELECTOR, "nav>ul a").click()
+menu_items=driver.find_elements(By.CSS_SELECTOR, "#menu li")
+menu_items_texts =[]
+for item in menu_items:
+    text = item.text
+    menu_items_texts.append(text)
+
+isKika=False
+for text in menu_items_texts:
+    if text=="Kika":
+        isKika=True
+
+# menu_items_texts['Kika'].click()
+# or;
+driver.find_element(By.CSS_SELECTOR,"#menu > div > ul > li:nth-child(2) > a").click()
+
+driver.find_element(By.ID, "name").send_keys("Cookie")
+driver.find_element(By.ID, "email").send_keys("cookie@gmail.com")
+driver.find_element(By.ID, "message").send_keys("this is a message")
+driver.find_element(By.CSS_SELECTOR, "['value'='send']").click()
+
+
+input("click")
+
+
+
+
